779bcaed557b250f9c57aaf51f42ed7553df7822
Evgeniya Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-02-26 23:24:02 +0300

    Initial Commit

---
+++ Right on Target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on Target
+//
+//  Created by Евгения Шевцова on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
