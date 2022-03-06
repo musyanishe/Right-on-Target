1a66af95ed65c67ae91ff3dd1b98ac5e21433683
Evgeniya Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-03-06 14:32:20 +0300

    разбор методов жизненного цикла view

--- Right on Target/ViewController.swift
+++ Right on Target/ViewController.swift
@@ -15,6 +15,38 @@ class ViewController: UIViewController {
     var number = 0
     var round = 1
     var points = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+//        создаем метку для вывода номера версии
+//        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+//        изменяем текст метки
+//        versionLabel.text = "Версия 1.1"
+//        добавляем метку в родительский view
+//        self.view.addSubview(versionLabel)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
